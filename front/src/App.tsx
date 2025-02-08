894c1b84abf63e96d29e3ff72c0883592659474c
Felipe Rohde <feliperohdee@gmail.com>
2025-02-08 10:26:53 -0300

    init

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+
+const App = () => {
+    return (
+        <>
+            <div className='flex flex-col justify-center items-center bg-slate-950 min-h-screen'>
+                <Button variant={'destructive'}>Oi</Button>
+            </div>
+        </>
+    );
+};
+
+export default App;
